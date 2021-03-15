fe2476ce972d0633136e743fc5521803f4e952e8
elvislara1 <elarac01@elpuig.xeill.net>
2021-03-15 19:40:39 +0100

    segundo

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  .get('/mensajes', (req, res) =>{
+    res.send("hello");
+  });
